40010849d62bc9ca5de6b37a448e83e50670960d
keishiro terashita <20153180g@gmail.com>
2023-02-17 15:43:42 +0900

    コピーしてきた

---
+++ ex/script.js
@@ -0,0 +1,198 @@
+const board = document.querySelector(".board");
+const turn_shower = document.querySelector(".turnShower");
+let turn = "white";
+let opponent = "black";
+const colors = {
+  white: "白",
+  black: "黒",
+};
+let row_now = "";
+let column_now = "";
+const direction = [
+  [1, 0],
+  [1, -1],
+  [0, -1],
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+];
+
+// セル検索
+function cell(row, column) {
+  return document.getElementById(`cell_${row}_${column}`);
+}
+// クラス判定
+function checkClass(target, className) {
+  return target.classList.contains(className);
+}
+
+// テーブル内に行を作成
+function makeRow() {
+  const row = document.createElement("tr");
+  board.appendChild(row);
+}
+
+// マスを一つ行に挿入
+function createCell(row, column, target) {
+  const cell = document.createElement("td");
+  cell.className = "cell";
+  cell.id = `cell_${row}_${column}`;
+  cell.setAttribute("row", row);
+  cell.setAttribute("column", column);
+  target.appendChild(cell);
+}
+
+// ひっくり返せるやつ取得
+function getCells(row, column, r, c) {
+  let cells = [];
+  while (0 <= row - r && row - r <= 7 && 0 <= column - c && column - c <= 7) {
+    const checked_cell = cell(row - r, column - c);
+    if (checkClass(checked_cell, `cell_${opponent}`)) {
+      cells.push(checked_cell);
+      row -= r;
+      column -= c;
+      continue;
+    } else if (checkClass(checked_cell, `cell_${turn}`)) {
+      return cells;
+    } else {
+      cells = [];
+      return cells;
+    }
+  }
+  cells = [];
+  return cells;
+}
+
+// おけるマスがあるか探す
+function searchPossibleArea() {
+  let result = false;
+  for (i = 0; i < 8; i++) {
+    for (j = 0; j < 8; j++) {
+      if (checkClass(cell(i, j), "cell_black")) {
+        continue;
+      }
+      if (checkClass(cell(i, j), "cell_white")) {
+        continue;
+      }
+      direction.forEach((array) => {
+        const possibleAreas = getCells(i, j, array[0], array[1]);
+        if (possibleAreas[0]) {
+          result = true;
+        }
+      });
+    }
+  }
+  return result;
+}
+
+// 石を置く処理
+function putStone(row, column) {
+  const target = cell(row, column);
+  target.classList.add(`cell_${turn}`);
+}
+
+// 石をひっくり返す処理
+function reversStone(target) {
+  target.classList.toggle("cell_black");
+  target.classList.toggle("cell_white");
+}
+
+// 番を切り替える処理
+function switchTurn() {
+  if (turn === "black") {
+    turn = "white";
+    opponent = "black";
+  } else {
+    turn = "black";
+    opponent = "white";
+  }
+  turn_shower.innerHTML = colors[turn];
+}
+
+// 置けないところクリックしたら
+function impossibleMessage() {
+  messageArea.innerHTML = "そこには置けません！";
+}
+
+// ゲーム終了処理
+function gameEnd() {
+  const messageArea = document.getElementById("messageArea");
+  const blacks = document.querySelectorAll(".cell_black");
+  const whites = document.querySelectorAll(".cell_white");
+  const score_black = blacks.length;
+  const score_white = whites.length;
+  let result = "";
+  if (score_black > score_white) {
+    result = "黒の勝ち";
+  } else if (score_black < score_white) {
+    result = "白の勝ち";
+  } else {
+    result = "引き分け";
+  }
+  const message = `黒のスコアは${score_black}点、白のスコアは${score_white}点<br>勝負は${result}！`;
+  messageArea.innerHTML = message;
+}
+
+// ゲームボードを生成
+for (let i = 0; i < 8; i++) {
+  makeRow();
+}
+const rows = document.querySelectorAll("tr");
+rows.forEach((row, index) => {
+  for (let i = 0; i < 8; i++) {
+    createCell(index, i, row);
+  }
+});
+
+putStone(3, 3);
+putStone(4, 4);
+switchTurn();
+putStone(3, 4);
+putStone(4, 3);
+
+// 各セルにイベント追加
+const cells = document.querySelectorAll(".cell");
+cells.forEach((cell) => {
+  const row = cell.getAttribute("row");
+  const column = cell.getAttribute("column");
+
+  cell.addEventListener("click", function () {
+    let correct = false;
+    // クリック時の処理
+    if (checkClass(this, "cell_black")) {
+      impossibleMessage();
+      return;
+    }
+    if (checkClass(this, "cell_white")) {
+      impossibleMessage();
+      return;
+    }
+    let targets = "";
+    // ひっくり返せる石を探す & ひっくり返す
+    direction.forEach((array) => {
+      targets = getCells(row, column, array[0], array[1]);
+      if (targets) {
+        targets.forEach((target) => {
+          reversStone(target);
+          correct = true;
+        });
+      }
+    });
+    if (correct) {
+      putStone(row, column);
+      messageArea.innerHTML = "";
+      switchTurn();
+      if (searchPossibleArea() === false) {
+        messageArea.innerHTML = `${colors[turn]}は置けません...`;
+        switchTurn();
+        if (searchPossibleArea() === false) {
+          gameEnd();
+        }
+      }
+    } else {
+      impossibleMessage();
+    }
+  });
+});
---
+++ hard/script.js
@@ -0,0 +1,169 @@
+const ruleArea = document.getElementById("rule");
+
+const rules = {
+  red: "赤い文字",
+  blue: "青い文字",
+  green: "緑の文字",
+  black: "黒い文字",
+  赤: "赤という文字",
+  青: "青という文字",
+  緑: "緑という文字",
+  黒: "黒という文字",
+  large: "大きい文字",
+  mid: "中間サイズの文字",
+  small: "小さい文字",
+};
+
+let isCorrect = "";
+
+const cells = document.querySelectorAll(".cell");
+const startButton = document.getElementById("start");
+const messageArea = document.getElementById("messageArea");
+let message = "";
+const letters = ["赤", "青", "緑", "黒"];
+const colors = ["red", "blue", "green", "black"];
+const size = ["large", "mid", "small"];
+let shown = [];
+let rule = "";
+let score = 0;
+let chain = 0;
+let chainRecord = 0;
+
+// 配列並び替え
+function shuffle(array) {
+  for (i = array.length - 1; i > 0; i--) {
+    r = Math.floor(Math.random() * (i + 1));
+    tmp = array[i];
+    array[i] = array[r];
+    array[r] = tmp;
+  }
+  return array;
+}
+
+// 配列からランダム取得
+function choseFrom(array) {
+  return shuffle(array)[0];
+}
+
+// どっちが大きい？
+function max(a, b) {
+  if (a > b) {
+    return a;
+  } else {
+    return b;
+  }
+}
+
+// ランダムにクラスつけるよ
+function addRandomClass(targets, classes) {
+  const randomClasses = shuffle(classes);
+  targets.forEach((target, index) => {
+    target.classList.add(randomClasses[index]);
+    shown.push(randomClasses[index]);
+  });
+}
+
+// 文字表示
+function showLetters() {
+  // 表示場所選ぶ
+  const targets = [];
+  const random = shuffle([0, 1, 2, 3, 4, 5]);
+  for (i = 0; i < 3; i++) {
+    target = document.getElementById(`cell_${random[i]}`);
+    targets.push(target);
+  }
+
+  // 文字きめる
+  const shuffledLetters = shuffle(letters);
+  targets.forEach((target, index) => {
+    target.innerHTML = shuffledLetters[index];
+    target.classList.add(shuffledLetters[index]);
+    shown.push(shuffledLetters[index]);
+  });
+  // クラスつける
+  addRandomClass(targets, colors);
+  addRandomClass(targets, size);
+}
+
+// 指示だし
+function showRule() {
+  const index = choseFrom(shown);
+  rule = rules[index];
+  ruleArea.innerHTML = rule;
+  isCorrect = index;
+}
+
+// リセット
+function reset() {
+  cells.forEach((cell) => {
+    cell.className = "cell";
+    cell.innerHTML = "";
+    ruleArea.innerHTML = "";
+    rule = "";
+    shown = [];
+  });
+}
+
+// 正誤判定
+function judge(target, isCorrect) {
+  return target.classList.contains(isCorrect);
+}
+
+// クリック時処理
+cells.forEach((cell) => {
+  cell.addEventListener("click", function () {
+    if (judge(cell, isCorrect)) {
+      // 正解の時
+      score++;
+      chain++;
+      if (chain === 1) {
+        message = "正解！";
+      } else {
+        message = `${chain}問連続正解！`;
+      }
+    } else {
+      // 不正解の時
+      chainRecord = max(chain, chainRecord);
+      chain = 0;
+      message = "不正解...";
+    }
+    messageArea.innerHTML = message;
+    // 次の問題
+    reset();
+    showLetters();
+    showRule();
+  });
+});
+
+// ゲーム終了
+function gameEnd() {
+  cells.forEach((cell) => {
+    cell.classList.add("no_event");
+    startButton.style.display = "block";
+  });
+  chainRecord = max(chain, chainRecord);
+  messageArea.innerHTML = `全部で${score}問正解！<br>最大連続正解数：${chainRecord}`;
+  reset();
+  score = 0;
+  chain = 0;
+  chainRecord = 0;
+  ruleArea.innerHTML = "startを押してね";
+  cells.forEach((cell) => {
+    cell.classList.add("no_event");
+  });
+}
+
+// スタートボタン
+startButton.addEventListener("click", function () {
+  reset();
+  messageArea.innerHTML = "";
+  startButton.style.display = "none";
+  showLetters();
+  showRule();
+  cells.forEach((cell) => {
+    cell.classList.remove("no_event");
+  });
+  window.setTimeout(function () {
+    gameEnd();
+  }, 30000);
+});
